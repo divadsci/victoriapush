ffbe9063aae553be5b5e1115bebe0ee17931a852
divadsci <david.a.entwistle@gmail.com>
2024-01-14 22:35:21 +0000

    Started work on processing received datapoints

--- victoriapush.go
+++ victoriapush.go
@@ -8,14 +8,42 @@ assign global labels
 package victoriapush
 
 type Vic struct {
-	URL          string
-	globalLabels map[string]string
+	URL          string            //URL of victoriametrics server
+	globalLabels map[string]string //labels to be applied to every metric pushed
+	maxWait      float64           //maximum time to wait before sending whatever is in buffer
+	maxQueueSize int               //maximum size for metrics queue before sending
+	msgQ         chan string       //queue of messages waiting to be pushed
+}
+
+type DataPoint struct {
+	metric string
+	value  float64
+	labels map[string]string
 }
 
 // add URL and globalLabels
-func (v *Vic) InitVictoria(URL string, globalLabels map[string]string) {
+func NewVictoriaPush(URL string, globalLabels map[string]string) *Vic {
+	v := new(Vic)
 	v.URL = URL
 	v.globalLabels = globalLabels
+	v.maxWait = 1
+	v.maxQueueSize = 10
+
+	//start victoriapush loop that sends data based on time limit or queue size
+	go v.pushLoop()
+	return v
+}
+
+// Set VictoriaMetrics server URL
+func (v *Vic) SetURL(URL string) {
+	//todo: do some checking here?
+	v.URL = URL
+}
+
+// Set the limits of the metric queue, maxWait for maximum wait for more data before sending, maxQueueSize for maximum number of metrics in push queue before sending
+func (v *Vic) SetQueueLimits(maxWait float64, maxQueueSize int) {
+	v.maxWait = maxWait
+	v.maxQueueSize = maxQueueSize
 }
 
 // Replace globalLabels map with a new one
@@ -26,12 +54,10 @@ func (v *Vic) ReplaceGlobalLabels(globalLabels map[string]string) {
 // Add a map of labels to globalLabels
 func (v *Vic) AddGlobalLabels(globalLabels map[string]string) {
 
-	//get globalLabels from context, iterate over new map and add each to globalLabels from ctx
-
+	//iterate over new globalLabels map and add each to Vic data
 	for label, val := range globalLabels {
 		v.globalLabels[label] = val
 	}
-
 }
 
 // Remove the list of labels from the globalLabels map IF it matches
@@ -41,4 +67,21 @@ func (v *Vic) RemGlobalLabels(labelsForRem []string) {
 	for _, label := range labelsForRem {
 		delete(v.globalLabels, label)
 	}
+}
+
+// Add dataPoint to queue ready to be pushed
+func (v *Vic) EnqueueDataPoint(dataPoint DataPoint) {
+	dataPointExpo := v.dataPointToExpo(dataPoint)
+	println(dataPointExpo)
+
+}
+
+// convert DataPoint to prometheus exposition format ready for pushing to server
+func (v *Vic) dataPointToExpo(dataPoint DataPoint) string {
+	return dataPoint.metric
+}
+
+// loop continually, push data to server if maxWait elapsed or maxQueueSize reached
+func (v *Vic) pushLoop() {
+
 }
